ee51ee34ab367626263a1304ef75f9c0196a2664
João Frugis <joaofrugis@nappsolutions.com>
2021-10-27 10:46:56 -0300

    Criado tela de listagem de eventos e migrations adicionadas

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.base import ModelState
 
 # Create your models here.
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
+    local = models.CharField(max_length=50, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -15,6 +17,8 @@ class Evento(models.Model):
     def __str__(self) -> str:
         return self.titulo
     
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%MHrs')    
 
 
 
--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import response
+from django.shortcuts import redirect, render, HttpResponse
+from core.models import Evento
 
 # Create your views here.
+
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.all()
+    # Filtrar por usuário
+    # evento = Evento.objects.filter(usuario=usuario)
+    data = {'eventos':evento}
+    return render(request, 'agenda.html', data)
+
+def local_evento(request, titulo):
+    evento = Evento.objects.get(titulo=titulo)
+    return HttpResponse('O local do evento é: {}'.format(evento.local))
+
+# Redireciona para /agenda
+# def index(request):
+#   return redirect('/agenda/')
